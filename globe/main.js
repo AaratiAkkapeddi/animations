8a2f45875034b8a878e1b9e6749125cd9a695b5b
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-10-08 23:44:03 -0400

    globe

--- globe/main.js
+++ globe/main.js
@@ -1,31 +1,58 @@
 
-var imgs = []
+/* Browser height and width for diff browsers */
 var w=window,
 d=document,
 e=d.documentElement,
 g=d.getElementsByTagName('body')[0],
 width=w.innerWidth||e.clientWidth||g.clientWidth,
 height=w.innerHeight||e.clientHeight||g.clientHeight;
+
+/* mapping function */
 function mapRange (value, a, b, c, d) {
-    // first map value from (a..b) to (0..1)
     value = (value - a) / (b - a);
-    // then map it from (0..1) to (c..d) and return it
     return c + value * (d - c);
 }
-
-for (var i = 181 - 1; i >= 0; i--) {
-	//Things[i]
-	var imgstring = "globe_0/"+ i + ".png"
-	imgs.push(imgstring)
+for (var i = 45 - 1; i >= 0; i--) {
+    var imgstring = "globe_0/"+ i + ".png";
+    var img = document.createElement("IMG");
+    img.classList.add('off')
+    img.id = "globe_0-"+i;
+    img.src = imgstring;
+    var wrapper = document.getElementById("wrapper")
+    wrapper.appendChild(img);
 }
-var img = document.getElementById('myimg')
+
+//var img = document.getElementById('myimg')
+
+// //Darkmode on click...
+
+// document.addEventListener("mousedown", function(e){
+// 	img.classList.add('dark')
+// })
+
+// document.addEventListener("mouseup", function(e){
+// 	img.classList.remove('dark')
+// })
+
+//map mouse to correct image
 document.addEventListener("mousemove", function(e){
 	var x = e.x;
 	var y = e.y;
-	x = Math.floor(mapRange(x, 0, width, 0, 181))
-
-
-	img.src = imgs[x]	
-	
-	
+	x = Math.floor(mapRange(x, 0, width, 0, 45))
+    y = Math.floor(mapRange(y, height, 0, 0, 10))
+    console.log(y)
+    if((x >=0 && x <= 180) && (y >= 0 && y <=9)){
+    	// img.src = "cube_"+y+"/"+x+".png"
+        var imgs = document.getElementsByTagName('img')
+        for (var i = imgs.length - 1; i >= 0; i--) {
+            imgs[i].classList.add('off')
+        }
+        var img = document.getElementById("globe_0"+"-"+x)
+        img.classList.remove('off')
+    }
 });
+
+window.onload = function(){
+    document.getElementById('loading').classList.add("off")
+    document.body.classList.remove('loading')
+}
